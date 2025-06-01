3082c30780129001441a1d9e1c4357b0424cad76
yermaalexx <yermakov80@gmail.com>
2025-06-01 12:23:12 +0300

    social-network

---
+++ src/main/java/com/yermaalexx/socialnetwork/GateApplication.java
@@ -0,0 +1,13 @@
+package com.yermaalexx.socialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GateApplication.class, args);
+	}
+
+}
